2c4606f22be79946e22446a426d7f78906f1aeef
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2022-12-10 19:05:10 +0530

    Add files via upload

---
+++ colorDiff.py
@@ -0,0 +1,4 @@
+color_list1=set(["White","Black","Pink","Red"])
+color_list2=set(["Black","Green","Blue","Red"])
+print("colors from color_list1 not contained in color_list2 is",
+      color_list1.difference(color_list2))
