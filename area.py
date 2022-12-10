527d5b9ced32d5e5e9ab72074b359c824a12f705
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2022-12-10 17:58:35 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+def area_of_circle(r):
+    pi=3.147
+    area=pi*r*r
+    return area
+radius=float(input("Enter the radius: "))
+print("Area of the circle for given radius is ",area_of_circle(radius))
